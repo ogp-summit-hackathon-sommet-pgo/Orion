304de64057a3c1864e2a22ef1c1d8f6e3e375787
Sayfullah Eid <sayfullaheid@gmail.com>
2019-05-28 11:49:52 -0400

    Added create_cost_df()
    Creates a pandas DataFrame of the cost of solar and green roofing for given area series
        :param area_series: Pandas series with roof areas
        :return: Pandas DataFrame with columns ['SOLAR_COST', 'GREEN_COST']

--- installCost.py
+++ installCost.py
@@ -26,8 +26,31 @@ def install_cost_green(area: float) -> int:
     return int(240 * int(area))
 
 
-if __name__ == "__main__":
-    f = lambda a: (install_cost_solar(float(a)))
-    massing_df = pandas.read_csv('DataSets/3DMassing_2018_WGS84_fixed.csv', index_col=['SHAPE_AREA'],
-                                 usecols=['SHAPE_AREA'])
+def create_cost_df(area_series: pandas.Series) -> pandas.DataFrame:
+    """
+    Creates a pandas DataFrame of the cost of solar and green roofing for given area series
+    :param area_series: Pandas series with roof areas
+    :return: Pandas DataFrame with columns ['SOLAR_COST', 'GREEN_COST']
+    """
+    count = int()
+    size = area_series.shape[0]
+    print('Generating area costs: ')
+    cost = []
+    for area in area_series:
+        print(count/size * 100, '%')
+        cost.append((install_cost_solar(area), install_cost_green(area)))
+        count += 1
+    cost_df = pandas.DataFrame(cost, columns=['SOLAR_COST', 'GREEN_COST'])
+    return cost_df
 
+
+if __name__ == "__main__":
+    massing_df = pandas.read_csv('DataSets/3DMassing_2018_WGS84_fixed.csv')
+    # cost = []
+    # for item in massing_df['SHAPE_AREA']:
+    #     cost.append(install_cost_solar(item))
+    # massing_df['SOLAR_COST'] = pandas.Series(cost)
+    costdf = create_cost_df(massing_df['SHAPE_AREA'])
+    massing_df['SOLAR_COST'] = costdf['SOLAR_COST']
+    massing_df['GREEN_COST'] = costdf['GREEN_COST']
+    print(massing_df)
